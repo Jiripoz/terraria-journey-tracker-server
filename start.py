f018e552a5325b8ac24cd8b30ef038b65b7283a0
Jiripoz <103076776+Jiripoz@users.noreply.github.com>
2022-12-20 02:56:32 -0300

    added cachorro.py as watchdog, moved log_setup.py from root to src

--- start.py
+++ start.py
@@ -1,10 +1,19 @@
 from global_configs import PLAYER_FILE_PATH, VERBOSE
-from log_setup import logger
+from src.log_setup import logger
 from src.char import get_char
+from src.cachorro import setup_watchdog
 
 logger.info("starting script")
 logger.debug(f"PLAYER_FILE_PATH: {PLAYER_FILE_PATH}")
 logger.debug(f"VERBOSE: {VERBOSE}")
 
-a = get_char()
-print(a.items_progress)
+
+def main_loop():
+    get_char(PLAYER_FILE_PATH)
+
+    # TODO: Juntar player com infos de items
+    # TODO: Apresentar
+    # - Apresentar pode ser escrever na tela, mandar pro websocket, qualquer coisa
+
+
+setup_watchdog(PLAYER_FILE_PATH, main_loop)
