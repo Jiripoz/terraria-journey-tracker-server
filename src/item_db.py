4655e7d676f91d0b2f91c227da7f22c2ebd9dfaf
Jiripoz <103076776+Jiripoz@users.noreply.github.com>
2022-12-23 06:46:42 -0300

    Revert "Develop"

---
+++ src/item_db.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+import json
+import os
+from os import path
+
+IMAGE_ROOT_URL = "https://static.wikia.nocookie.net/terraria_gamepedia/images/"
+WIKI_ROOT_URL = "https://terraria.fandom.com/wiki/"
+ITEMS_JSON_PATH = str(path.join(path.abspath(os.getcwd()), "data", "items.json"))
+
+
+@dataclass
+class Item:
+    id: str
+    name: str
+    internal_name: str
+    wiki_url: str
+    image_url: str
+    category: str
+    research_needed: int
+
+    def __str__(self):
+        return f"[{self.id}] {self.name} - {self.category} | Needs {self.research_needed} for full research"
+
+
+class ItemDB:
+    def __init__(self, items: list):
+        self.items_dict = {}
+
+        for item in items:
+            self.items_dict[item["id"]] = Item(
+                id=item["id"],
+                name=item["name"],
+                internal_name=item["internalName"],
+                wiki_url=WIKI_ROOT_URL + item["itemUrl"],
+                image_url=IMAGE_ROOT_URL + item["imageUrl"],
+                category=item["category"],
+                research_needed=item["research"],
+            )
+
+    def get_item(self, item_id: int) -> Item:
+        if item_id in self.items_dict:
+            return self.items_dict[item_id]
+        return None
+
+    def get_all_items(self) -> list:
+        return list(self.items_dict.values())
+
+    def get_all_items_by_category(self, category: str) -> list:
+        return [item for item in self.items_dict.values() if item.category == category]
+
+    # Kinda slow
+    def get_item_by_internal_name(self, internal_name: str) -> Item:
+        return [
+            item
+            for item in self.items_dict.values()
+            if item.internal_name == internal_name
+        ][0]
+
+
+with open(ITEMS_JSON_PATH) as f:
+    items = json.load(f)
+    item_db = ItemDB(items=items)
--- start.py
+++ start.py
@@ -1,2 +1,19 @@
-print('carlinhos e jurandir')
-print('brigaram por conta de falta de queijo')
+from global_configs import PLAYER_FILE_PATH, VERBOSE
+from src.log_setup import logger
+from src.char import get_char
+from src.cachorro import setup_watchdog
+
+logger.info("starting script")
+logger.debug(f"PLAYER_FILE_PATH: {PLAYER_FILE_PATH}")
+logger.debug(f"VERBOSE: {VERBOSE}")
+
+
+def main_loop():
+    player = get_char(PLAYER_FILE_PATH)
+    player.print_progress_overview()
+    player.print_partially_researched()
+
+
+main_loop()
+
+setup_watchdog(PLAYER_FILE_PATH, main_loop)
